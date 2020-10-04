1f03f20db2f4692a1639aba919688d658d9afb14
Alus454 <alukard454@gmail.com>
2020-10-04 15:41:38 +0200

    I do somthing and working!!

--- src/main/java/pl/yellowduck/netflix90/gui/CategoriesController.java
+++ src/main/java/pl/yellowduck/netflix90/gui/CategoriesController.java
@@ -2,9 +2,19 @@ package pl.yellowduck.netflix90.gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import pl.yellowduck.netflix90.films.Category;
 import pl.yellowduck.netflix90.films.CategoryHibernate;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class CategoriesController {
 
@@ -12,8 +22,26 @@ public class CategoriesController {
     TableView<CategoryHibernate> categoriesTable;
 
     public void loadData(ActionEvent event) {
-        categoriesTable.getItems()
-                .add(new CategoryHibernate("TEST", "TEST DESCRPTION"));
-        categoriesTable.refresh();
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(CategoryHibernate.class)
+                .buildSessionFactory();
+        System.out.println("\n\n--------------------->\n" +
+                "Hibernate Session Factory Created");
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+
+            TypedQuery<CategoryHibernate> query = session.createQuery("SELECT c from CategoryHibernate c", CategoryHibernate.class);
+            categoriesTable.getItems().clear();
+            List<CategoryHibernate> reultList = query.getResultList();
+            categoriesTable.getItems().addAll(reultList);
+            tx.commit();
+        } catch (Exception ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Fill in all fielnds");
+            alert.show();
+
+        }
     }
 }
--- src/main/java/pl/yellowduck/netflix90/gui/CategoryController.java
+++ src/main/java/pl/yellowduck/netflix90/gui/CategoryController.java
@@ -1,4 +1,58 @@
 package pl.yellowduck.netflix90.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import pl.yellowduck.netflix90.films.CategoryHibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
 public class CategoryController {
+
+
+    @FXML
+    TextField nameinput;
+
+    @FXML
+    TextField descriptioninput;
+
+    public void save(ActionEvent actionEvent) {
+        String name = nameinput.getText();
+        String description = descriptioninput.getText();
+        if (StringUtils.isNoneBlank(name) && StringUtils.isNoneBlank(description)){
+            SessionFactory sessionFactory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(CategoryHibernate.class)
+                    .buildSessionFactory();
+            System.out.println("\n\n--------------------->\n" +
+                    "Hibernate Session Factory Created");
+            Transaction tx = null;
+            try (Session session = sessionFactory.openSession()) {
+                tx = session.beginTransaction();
+                CategoryHibernate category = new CategoryHibernate(name, description);
+                session.save(category);
+                tx.commit();
+            } catch (HibernateException ex) {
+                if (tx != null && !tx.getRollbackOnly()) {
+                    tx.rollback();
+                }
+                throw ex;
+            }
+        }else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Fill in all fielnds");
+            alert.show();
+        }
+
+
+
+    }
 }
